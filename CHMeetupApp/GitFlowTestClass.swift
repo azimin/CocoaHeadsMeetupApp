9b32833a978102c41a7e611eef76ccb94477f591
Alexander Zimin <azimin@me.com>
2017-02-20 21:47:40 +0300

    added gitflow test class

---
+++ CHMeetupApp/GitFlowTestClass.swift
@@ -0,0 +1,14 @@
+//
+//  GitFlowTestClass.swift
+//  CHMeetupApp
+//
+//  Created by Alexander Zimin on 20/02/2017.
+//  Copyright © 2017 CocoaHeads Comunity. All rights reserved.
+//
+
+import Foundation
+
+// TODO: - Remove it
+class GitFlowTestClass {
+  
+}
