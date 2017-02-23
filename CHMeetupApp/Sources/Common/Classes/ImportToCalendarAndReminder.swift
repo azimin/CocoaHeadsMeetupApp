be0d3fe279a6a54ccfe3228c1e74f453d952e4f7
Egor Petrov <petrov.bz.molod@gmail.com>
2017-02-23 17:58:15 +0300

    [Vuq6BkBC] Wrote calendar and reminder modules

--- CHMeetupApp/Sources/Common/Classes/ImportToCalendarAndReminder.swift
+++ CHMeetupApp/Sources/Common/Classes/ImportToCalendarAndReminder.swift
@@ -9,14 +9,33 @@
 import EventKit
 
 class ImportTo {
-  private let eventStore = EKEventStore()
-  
-  func importToCalendar(title: String, location: String, startTime: String, endTime: String){
+  func ToCalendar(eventStore: EKEventStore, title: String, location: String, startTime: String, endTime: String) {
+    eventStore.requestAccess(to: .event, completion: {(_, _) in})
     let event = EKEvent(eventStore: eventStore)
     event.title = title
     event.location = location
-    event.startDate = Date(timeIntervalSince1970: startTime.dateFrom)
-    event.endDate = Date(timeIntervalSince1970: endTime.dateFrom)
-    
+    event.startDate = Date(timeIntervalSince1970: startTime.dateSince)
+    event.endDate = Date(timeIntervalSince1970: endTime.dateSince)
+    event.calendar = eventStore.defaultCalendarForNewEvents
+    do {
+      try eventStore.save(event, span: .thisEvent)
+    } catch {
+      print("Sorry")
+    }
+  }
+  func ToReminder(eventStore: EKEventStore, title: String, location: String, startTime: String, endTime: String) {
+    eventStore.requestAccess(to: .reminder, completion: {(_, _) in })
+    let reminder = EKReminder(eventStore: eventStore)
+    reminder.title = title
+    reminder.location = location
+    reminder.isCompleted = false
+    reminder.dueDateComponents = DateComponents(year: 2017, month: 02, day: 24, hour: 11, minute: 00)
+    reminder.calendar = eventStore.defaultCalendarForNewReminders()
+    do {
+      try eventStore.save(reminder, commit: true)
+      print("okay")
+    } catch {
+      print("Sorry")
+    }
   }
 }
