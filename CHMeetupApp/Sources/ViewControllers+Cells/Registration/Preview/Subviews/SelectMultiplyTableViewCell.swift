1afaec9c7186f3da8a0f573e1550bc65f0208721
GlobakMaxim <maxim.globak@gmail.com>
2017-03-05 23:14:55 +0300

    [Hr1pcj2p] hotfix error

--- CHMeetupApp/Sources/ViewControllers+Cells/Registration/Preview/Subviews/SelectMultiplyTableViewCell.swift
+++ CHMeetupApp/Sources/ViewControllers+Cells/Registration/Preview/Subviews/SelectMultiplyTableViewCell.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class SelectMultiplyTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
-    
+    // Configure the view for the selected state
+  }
+
 }
 
 // MARK: - ReusableCell
 extension SelectMultiplyTableViewCell: ReusableCell {
-  
+
   static var identifier: String {
     return String(describing: self)
   }
-  
+
   static var nib: UINib? {
     return UINib(nibName: String(describing: self), bundle: nil)
+  }
 }
