a0153fded898233cb940ec0041b79539de5de636
Sam Mejlumyan <smejlum@gmail.com>
2017-03-07 22:57:48 +0300

    [oglMVpHl] Created EventRegForm plain object

---
+++ CHMeetupApp/Sources/Model/PO/Event/RegForm/EventRegFormFieldPlainObject.swift
@@ -0,0 +1,49 @@
+//
+//  EventRegFormFieldPlainObject.swift
+//  CHMeetupApp
+//
+//  Created by Sam Mejlumyan on 07/03/2017.
+//  Copyright © 2017 CocoaHeads Comunity. All rights reserved.
+//
+
+import Foundation
+
+struct EventRegFormFieldPlainObject {
+
+    enum EventRegFormFieldType: String {
+        case `string`
+        case checkbox
+        case radio
+
+    }
+
+    let id: Int
+    let required: Bool
+    let name: String
+    let type: EventRegFormFieldType
+    let answers: [EventRegFormFieldAnswerPlainObject]
+
+}
+
+extension EventRegFormFieldPlainObject: PlainObjectType {
+
+    init?(json: JSONDictionary) {
+
+        guard
+            let id = json["field_id"] as? Int,
+            let required = json["required"] as? Bool,
+            let field = json["field"] as? JSONDictionary,
+            let name = field["name"] as? String,
+            let fieldTypeString = field["type"] as? String,
+            let fieldType = EventRegFormFieldType(rawValue: fieldTypeString),
+            let fieldAnswers = field["field_answers"] as? [JSONDictionary]
+            else { return nil }
+
+        self.id = id
+        self.required = required
+        self.name = name
+        self.type = fieldType
+        self.answers = fieldAnswers.flatMap(EventRegFormFieldAnswerPlainObject.init)
+    }
+
+}
