f4a63e3f08499ea45c91e1c4b4fa2d2761f8424c
Andrey Konstantinov <8ofproject@gmail.com>
2017-03-16 01:08:54 +0300

    Add classes

---
+++ CHMeetupApp/Sources/Common/Views/RadioCell/RadioTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RadioTableViewCell.swift
+//  CHMeetupApp
+//
+//  Created by Andrey Konstantinov on 16/03/2017.
+//  Copyright © 2017 CocoaHeads Community. All rights reserved.
+//
+
+import UIKit
+
+final class RadioTableViewCell: PlateTableViewCell {
+
+}
---
+++ CHMeetupAppTests/Views/RadioTableViewCellTests.swift
@@ -0,0 +1,13 @@
+//
+//  RadioTableViewCellTests.swift
+//  CHMeetupApp
+//
+//  Created by Andrey Konstantinov on 16/03/2017.
+//  Copyright © 2017 CocoaHeads Community. All rights reserved.
+//
+
+import XCTest
+
+final class RadioTableViewCellTests: XCTestCase {
+
+}
