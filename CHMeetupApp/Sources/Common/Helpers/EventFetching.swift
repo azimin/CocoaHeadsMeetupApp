8e679a03326944e755fa00f7795435618c57184f
Kirill Averyanov <Kirillzzy@gmail.com>
2017-04-06 17:41:55 +0300

    [XQgNIbD5] Fetching logic

---
+++ CHMeetupApp/Sources/Common/Helpers/EventFetching.swift
@@ -0,0 +1,21 @@
+//
+//  EventFetching.swift
+//  CHMeetupApp
+//
+//  Created by Kirill Averyanov on 06/04/2017.
+//  Copyright © 2017 CocoaHeads Community. All rights reserved.
+//
+
+import Foundation
+
+struct EventFetching: FetchingElements {
+  static func fetchElements(request: Request<[EventPlainObject]>, completion: (() -> Void)? = nil) {
+    Server.standard.request(request, completion: { list, error in
+      guard let list = list,
+        error == nil else { return }
+
+      EventPlainObjectTranslation.translate(of: list, to: nil)
+      if let completion = completion { completion() }
+    })
+  }
+}
